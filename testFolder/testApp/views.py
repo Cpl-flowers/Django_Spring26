07c2a04cd5be32e1fed0b3c8caf36d520bec435c
Angel Rizo <ra072006@gmail.com>
2026-03-26 02:12:15 -0500

    Implemented django user authentication system, made a basic home page, and styled login/signup pages

--- testFolder/testApp/views.py
+++ testFolder/testApp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def home(request):
+    return render(request, 'testApp/home.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'testApp/signup.html', {'form': form})
