ea857b6c3f9d1fe034dfd8af659c50b7fdcb5ecb
Arly Garcia <arly.garcia02@utrgv.edu>
2026-04-05 19:40:29 -0500

    Stop tracking virtual environment and chae files

--- testFolder/testApp/views.py
+++ testFolder/testApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Building
 
-# Create your views here.
+
+def home(request):
+    buildings = Building.objects.all()
+    return render(request, 'testApp/home.html', {'buildings': buildings})
+
+
+def login_view(request):
+    return render(request, 'testApp/login.html')
+
+
+def signup_view(request):
+    return render(request, 'testApp/login.html')
