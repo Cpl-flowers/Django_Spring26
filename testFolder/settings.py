af39913bfc0396e5d825b6dfca214ad0f88077de
Cpl-flowers <kevingabrielflores@hotmail.com>
2026-04-13 17:18:04 -0500

    finished merging angel's branch and resolved bugs

--- testFolder/settings.py
+++ testFolder/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'testApp'
+    'testFolder.testApp'
     # 'members'
 ]
 
--- testFolder/urls.py
+++ testFolder/urls.py
@@ -20,11 +20,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
-from testApp import views
+from testFolder.testApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='Login'),
+    path('login/', auth_views.LoginView.as_view(template_name='testApp/login.html'), name='Login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='Logout'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', views.home, name='Home'),
